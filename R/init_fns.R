fe6bb1a38127bbc3948fc808a4d214ffc2a6ff60
Marc Carlson <mcarlson@fhcrc.org>
2008-10-28 21:22:19 +0000

    Adds gcspikelite to the repos.
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/gcspikelite@736 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/init_fns.R
@@ -0,0 +1,19 @@
+############################################################
+##
+## file: init_fns.R
+##
+## Mark Robinson (mrobinson@wehi.edu.au)
+## Created 07 Oct 2007
+##
+##
+############################################################
+
+
+.First.lib <- function(libname, pkgname) {
+  s <- search() 
+  
+  #require(MassSpecWavelet,quietly = TRUE, warn.conflicts = TRUE)
+  #require(gplots,quietly = TRUE, warn.conflicts = TRUE)
+  #require(preprocessCore,quietly = TRUE, warn.conflicts = TRUE)
+
+}
